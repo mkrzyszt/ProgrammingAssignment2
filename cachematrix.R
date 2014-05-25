977eb6b7b3f83f7428994305e7829330f136014d
Mark K <mkrzyszt@gmail.com>
2014-05-25 11:29:41 -0700

    Completed Matrix Solver

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to calculate and then subsequently cache the inverse
+## of a matrix. Function first check to see if matrix inverse exists
+## in the cache before calculating
 
-## Write a short comment describing this function
 
+## Function which generates a list of functions for the inverse solver
+## to check through. Ensures that inverse of matrix does not already 
+## exist in the cache before computing it again.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  # Sets variable into parent environment
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  # Checks parent environment for existing variable
+  get <- function() x
+  #function that solves the inverse of hte matrix
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## First checks to see if inverse of inputted matrix exists. If not,
+## calculates inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Checks parent environment for inverse
+  m <- x$getInverse()
+  #If inverse exists, exit and return inverse
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  #Get the matrix to be computed, and solve
+  data <- x$get()
+  m <- solve(data, ...)
+  #Sets solution into cache
+  x$setInverse(m)
+  m
 }
